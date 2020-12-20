a12b93853f66f874202eb827af31b6a8bfd60e77
Frank1963-mpoyi <frankmpoyi63@gmail.com>
2020-12-20 23:12:37 +0200

    login

--- ecommerce/apps/system/accounts/views.py
+++ ecommerce/apps/system/accounts/views.py
@@ -6,7 +6,9 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import Customer, Tag, Product, Order
 from .forms import OrderForm, CreateUserForm
 from .filters import OrderFilter
+from django.contrib import messages
 
+from django.contrib.auth import authenticate, login, logout
 
 
 
@@ -18,6 +20,10 @@ def registerPage(request):
         form = CreateUserForm(request.POST or None)
         if form.is_valid():
             form.save()
+            #just to get a user name to display in the message
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Account was created for '+ user)
+            return redirect('login')
     context ={
         'form': form
     }
@@ -25,6 +31,20 @@ def registerPage(request):
 
 
 def loginPage(request):
+    if request.method == "POST":
+        # let grap username and password from userin frontend
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        #after getting username and password let authenticate
+        user = authenticate(request, username=username, password=password)
+        #before authenticate let check if user is there
+        if user is not None:
+            login(request, user)#we will login this user
+            return redirect('home')
+        else:
+            messages.info(request, "Username OR Passowrd is incorrect")
+    
     template_name = "accounts/login.html"
     context = {
         
