ffbb2db7c669af80f8ab0f3d76dc5b6f34d55597
Frank1963-mpoyi <frankmpoyi63@gmail.com>
2020-12-20 23:25:01 +0200

    new login update

---
+++ static/accounts/js/login.js
@@ -0,0 +1,10 @@
+var form_fields = document.getElementsByTagName('input')
+form_fields[1].placeholder='Username..';
+
+form_fields[2].placeholder='Enter password...';
+
+
+for (var field in form_fields){
+    form_fields[field].className += 'form-control'
+}
+
